064c0e5cc72f674e47f625176f62e078cba26a32
MRiazIsr <markriaz13@gmail.com>
2024-01-06 19:22:15 +0200

    SP-2_Finished go to products section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,14 @@ const Home = () => {
     return (
     <div class={styles.main}>
       <div class={styles.box_to_orders}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in ipsum eget quam rhoncus faucibus. Donec eu vehicula nulla. Vestibulum lacus erat, pretium et eleifend eu, iaculis eu nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+        <h1>
+          PRO.beauty
+        </h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Mauris in ipsum eget quam rhoncus faucibus. 
+        Donec eu vehicula nulla. 
+        Vestibulum lacus erat, pretium et eleifend eu, iaculis eu nulla. 
+        Interdum et malesuada fames ac ante ipsum primis in faucibus. 
         <input type="button" class={styles.button} value="Products"></input>
       </div>
     </div>
