0f33e21998a9879fcbc8d4208aa88f898e687295
MRiazIsr <markriaz13@gmail.com>
2024-01-07 22:50:16 +0200

    css main hotfixes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,6 +12,11 @@ const Home = () => {
         Donec eu vehicula nulla. 
         Vestibulum lacus erat, pretium et eleifend eu, iaculis eu nulla. 
         Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Mauris in ipsum eget quam rhoncus faucibus. 
+        Donec eu vehicula nulla. 
+        Vestibulum lacus erat, pretium et eleifend eu, iaculis eu nulla. 
+        Interdum et malesuada fames ac ante ipsum primis in faucibus. 
         <input type="button" class={styles.button} value="Products"></input>
       </div>
     </div>
